a29bad1d67641f424c2078cd8694490296ed167e
fenglixiong <fenglixiong123@163.com>
2019-05-27 16:17:03 +0800

    添加时间函数

--- src/page/time/index.vue
+++ src/page/time/index.vue
@@ -2,12 +2,38 @@
   <div>
     Time
     <hr/>
+    <button @click="openWindows">打开百度之窗</button>
+    <button @click="formatTime">formatTime</button>
+    <button @click="friendlyTime">friendlyTime</button>
+    <br/>
+    <br/>
+    {{time}}
   </div>
 </template>
 
 <script>
+  import openWindow from '@/utils/window'
+  import {parseTime,formatTime} from '../../utils/time'
     export default {
-        name: "Time"
+      name: "Time",
+      data(){
+        return{
+          time:new Date()
+        }
+      },
+      methods:{
+        openWindows:function () {
+          openWindow("http://www.baidu.com","百度下哈",800,500);
+        },
+        formatTime:function(){
+          console.log("format time")
+          this.time = parseTime(new Date());
+        },
+        friendlyTime:function () {
+          console.log("show time")
+          this.time = formatTime(new Date().getTime()-60*60*1000,'{y}/{m}/{d} {h}:{i}:{s}');
+        }
+      }
     }
 </script>
 
